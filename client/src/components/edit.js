bf03bd29b4c6a6274b35dfbb30fca1da8702606a
Ryan Hung <rh.lh.314@gmail.com>
2024-01-12 23:27:49 -0800

    Framework done

--- client/src/components/edit.js
+++ client/src/components/edit.js
@@ -128,3 +128,49 @@ import { useParams, useNavigate } from "react-router";
    </div>
  );
 }
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const editedPerson = {
+      name: form.name,
+      position: form.position,
+      level: form.level,
+    };
+  
+    // This will send a post request to update the data in the database.
+    await fetch(`http://localhost:5000/update/${params.id}`, {
+      method: "POST",
+      body: JSON.stringify(editedPerson),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+  
+    navigate("/");
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id.toString();
+      const response = await fetch(`http://localhost:5000/record/${params.id.toString()}`);
+  
+      if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+  
+      const record = await response.json();
+      if (!record) {
+        window.alert(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+  
+      setForm(record);
+    }
+  
+    fetchData();
+  
+    return;
+  }, [params.id, navigate]);
