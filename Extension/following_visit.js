a5eb4f8f59f2509c54fa92cdbee0dbbdb5256e03
Ryan Hung <rh.lh.314@gmail.com>
2024-01-13 12:51:40 -0800

    Finished 1 dropdown in following_visit

--- Extension/following_visit.js
+++ Extension/following_visit.js
@@ -1,4 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
+    var ageButton = document.getElementById("age_button"); //copy lines 2-13 for every dropdown menu
+    var ageContent = document.getElementById("age_content");
+
+    ageButton.addEventListener("click", function(){
+        toggleDropdown("age")
+    });
+    
+    ageContent.addEventListener("click", function (event) {
+        if (event.target.tagName === "A") {
+            selectOptionAge(event.target.dataset.option); //make a new function for each drop down menu
+        }
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    window.addEventListener("click", function (event) {
+        if (!event.target.matches('.dropbtn')) {
+            var ageDropdown = document.getElementById("age"); //make a list of all dropdowns here and close all of them
+            ageDropdown.classList.remove("open"); //call the remove function for every single dropdown variable
+        }
+    });
+
     var goBackButton = document.getElementById('goBackButton');
     
     if (goBackButton) {
@@ -9,3 +30,31 @@ document.addEventListener('DOMContentLoaded', function () {
         window.history.back();
     }
 });
+function toggleDropdown(id) {
+    var dropdown = document.getElementById(id);
+    dropdown.classList.toggle("open");
+
+    var dropdownContent = document.getElementById(id + "_content");
+    dropdownContent.style.width = dropdown.offsetWidth + "px";
+}
+
+var age;
+
+function selectOptionAge(option) {
+    var dropdownButton = document.getElementById("age_button"); //make sure id matches drop down menu
+    dropdownButton.innerText = option;
+    toggleDropdown("age"); // Close the dropdown after selecting an option
+
+    switch (option) {
+        case '0 - 4 Years':
+            age = "0-4";
+            break;
+        case '5 - 11 Years':
+            age = '5-11';
+            break;
+        case '12+ Years':
+            age = '12+';
+            break;
+    }
+
+}
